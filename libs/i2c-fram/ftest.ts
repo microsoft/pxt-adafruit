715771b991ef908b63c40c5f21c69b61370b1dfe
Michal Moskal <michal.moskal@microsoft.com>
2016-04-05 18:21:15 -0700

    Add readme to i2c fram module

--- libs/i2c-fram/ftest.ts
+++ libs/i2c-fram/ftest.ts
@@ -8,7 +8,9 @@ function toBuf(arr: number[]) {
     return buf
 }
 
-i2c_fram.writeBuffer(98, toBuf([1,2,3,4,5,6,7]))
+i2c_fram.writeBuffer(98, toBuf([1, 2, 3, 4, 5, 6, 7]))
 
 console.log("100:" + i2c_fram.readByte(100))
 console.log("101:" + i2c_fram.readByte(101))
+
+

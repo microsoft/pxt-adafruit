2cc0523f9af9d8cc57b1b575e0aef4e3ce28497f
Shigeru KANEMOTO <sgk@switch-science.com>
2016-08-18 22:00:43 +0900

    Fix changeTempo() calculation

--- libs/microbit/music.ts
+++ libs/microbit/music.ts
@@ -162,7 +162,7 @@ namespace music {
     //% help=music/change-tempo weight=39
     //% blockId=device_change_tempo block="change tempo by (bpm)|%value" blockGap=8
     export function changeTempoBy(bpm: number): void {
-        setTempo(beat(BeatFraction.Whole) + bpm);
+        setTempo(beatsPerMinute + bpm);
     }
 
     /**

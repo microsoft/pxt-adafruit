718cc9d530437a65753eca16d83bd2a05b579811
Michal Moskal <michal@moskal.me>
2016-08-19 13:46:30 +0300

    Add script for running tests as in travis

---
+++ travis-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+set -x
+export KS_FORCE_CLOUD=yes
+(cd libs/lang-test0; node ../../node_modules/pxt-core/built/pxt.js run)
+(cd libs/lang-test1; node ../../node_modules/pxt-core/built/pxt.js run)
+(cd libs/lang-test0; node ../../node_modules/pxt-core/built/pxt.js test)
+(cd libs/lang-test1; node ../../node_modules/pxt-core/built/pxt.js test)
+node node_modules/pxt-core/built/pxt.js testdir tests
+(cd libs/hello; node ../../node_modules/pxt-core/built/pxt.js testconv https://az851932.vo.msecnd.net/files/td-converter-tests-v0.json)

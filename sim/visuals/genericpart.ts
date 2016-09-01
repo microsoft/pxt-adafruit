1f3a2ab6fe7af9738bc1fc0cad6c97b0e8b9b909
darzu <daryl.zuniga@gmail.com>
2016-08-31 19:37:20 -0700

    adds generic parts to instructions parts list and requirements panels

--- sim/visuals/genericpart.ts
+++ sim/visuals/genericpart.ts
@@ -1,18 +1,23 @@
 
 namespace pxsim.visuals {
+    export function mkGenericPartSVG(partVisual: PartVisualDefinition): SVGAndSize<SVGImageElement> {
+        let imgAndSize = mkImageSVG({
+            image: partVisual.image,
+            width: partVisual.width,
+            height: partVisual.height,
+            imageUnitDist: partVisual.pinDist,
+            targetUnitDist: PIN_DIST
+        });
+        return imgAndSize;
+    }
+
     export class GenericPart implements IBoardComponent<any> {
         public style: string = "";
         public element: SVGElement;
         defs: SVGElement[] = [];
 
         constructor(partVisual: PartVisualDefinition) {
-            let imgAndSize = mkImageSVG({
-                image: partVisual.image,
-                width: partVisual.width,
-                height: partVisual.height,
-                imageUnitDist: partVisual.pinDist,
-                targetUnitDist: PIN_DIST
-            });
+            let imgAndSize = mkGenericPartSVG(partVisual);
             let img = imgAndSize.el;
             let scaleFn = mkScaleFn(partVisual.pinDist, PIN_DIST);
             let [pinX, pinY] = partVisual.firstPin;

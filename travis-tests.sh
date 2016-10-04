693a6ba8a92bcb5059224d4fd8550cce6e94b0a7
Michal Moskal <michal@moskal.me>
2016-10-04 08:20:30 -0700

    Remove 3 converter tests with overflowing numbers

--- travis-tests.sh
+++ travis-tests.sh
@@ -7,4 +7,4 @@ export KS_FORCE_CLOUD=yes
 (cd libs/lang-test0; node ../../node_modules/pxt-core/built/pxt.js test)
 (cd libs/lang-test1; node ../../node_modules/pxt-core/built/pxt.js test)
 node node_modules/pxt-core/built/pxt.js testdir tests
-(cd libs/hello; node ../../node_modules/pxt-core/built/pxt.js testconv https://az851932.vo.msecnd.net/files/td-converter-tests-v0.json)
+(cd libs/hello; node ../../node_modules/pxt-core/built/pxt.js testconv https://az851932.vo.msecnd.net/files/td-converter-tests-v1.json)

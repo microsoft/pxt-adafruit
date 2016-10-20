b4452064c08dfecd4120b82a9abbfb2e6ae8959a
Michal Moskal <michal@moskal.me>
2016-10-20 14:49:51 +0100

    Add control.panic()

---
+++ libs/core/control.ts
@@ -0,0 +1,13 @@
+/**
+* Runtime and event utilities.
+*/
+//% weight=1 color="#333333"
+//% advanced=true
+namespace control {
+
+    /**
+     * Display specified error code and stop the program.
+     */
+    //% shim=pxtrt::panic
+    export function panic(code: number) { }
+}

2e3cdee2a29442dfae8e79ed9b0aa42477140eb2
Peli de Halleux <jhalleux@microsoft.com>
2016-10-24 16:34:33 -0700

    added deprecated radio api test

---
+++ tests/deprecated-apis.ts
@@ -0,0 +1,9 @@
+// deprecated APIs
+radio.receivedNumber();
+radio.receivedSerial();
+radio.receivedSignalStrength();
+radio.receivedString();
+radio.receivedTime();
+radio.onDataReceived(() => {
+    
+});

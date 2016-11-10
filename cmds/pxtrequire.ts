073731355dd17e18c8f9720b8bcf6f94cae7556f
Peli de Halleux <jhalleux@microsoft.com>
2016-11-10 14:20:40 -0800

    adding pxtrequire

---
+++ cmds/pxtrequire.ts
@@ -0,0 +1,4 @@
+import * as path from "path";
+export let pxtCore = require("pxt-core");
+// require.resolve() gives path to [pxt dir]/built/pxt.js, so move up twice to get pxt root dir
+export let pxtCoreDir = path.resolve(require.resolve("pxt-core"), "..", "..");

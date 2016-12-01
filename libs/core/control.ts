68154f12df593396f2182264c74351d24845b88a
Michal Moskal <michal@moskal.me>
2016-12-01 14:27:49 -0800

    Comment out assert

--- libs/core/control.ts
+++ libs/core/control.ts
@@ -8,13 +8,13 @@ namespace control {
     /**
      * Display specified error code and stop the program.
      */
-    //% shim=pxtrt::panic
+    // shim=pxtrt::panic
     export function panic(code: number) { }
 
     /**
      * Display specified error code and stop the program.
      */
-    //% shim=pxtrt::assert
+    // shim=pxtrt::assert
     export function assert(cond: boolean, code: number) { }
 }
 

93d90a2bde3e9c15d3789030e321acc63b01edc4
Sam El-Husseini <samelh@microsoft.com>
2017-01-03 12:14:25 -0800

    Few extra typing fixes.

--- cmds/pxtrequire.ts
+++ cmds/pxtrequire.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/pxt-core/typings/node/node.d.ts"/>
+/// <reference path="../node_modules/pxt-core/typings/globals/node/index.d.ts"/>
 /// <reference path="../node_modules/pxt-core/built/pxtlib.d.ts" />
 
 import * as path from "path";

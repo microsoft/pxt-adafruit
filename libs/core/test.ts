6643604344630184db979ed93d58c01a34844a92
Michal Moskal <michal@moskal.me>
2017-01-11 11:54:41 +0000

    Updating test script

--- libs/core/test.ts
+++ libs/core/test.ts
@@ -1,14 +1,22 @@
-buttons.left.onPressed(() => {
-        pins.LEDRX.digitalWrite(0)
-})
+let v0 = 0
+let v1 = 0
 buttons.right.onPressed(() => {
-        pins.LEDTX.digitalWrite(0)
+        pins.LEDRX.digitalWrite(v0)
+        v0 = v0 ? 0 : 1
+})
+buttons.left.onPressed(() => {
+        pins.LEDTX.digitalWrite(v1)
+        v1 = v1 ? 0 : 1
 })
 
 pins.LED.digitalWrite(1)
 
 let i = 0
 while (true) {
-     serial.writeValue("X", i++);
-     basic.pause(500);
+        if (buttons.slide.isPressed())
+                pins.LED.digitalWrite(1)
+        else
+                pins.LED.digitalWrite(0)
+        serial.writeValue("T", input.temperature());
+        basic.pause(500);
 }

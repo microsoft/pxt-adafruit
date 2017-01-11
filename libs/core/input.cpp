c225447204396b0ca7c6e24e368c8e57082e4189
Peli de Halleux <jhalleux@microsoft.com>
2017-01-10 16:37:26 -0800

    renabled constants

--- libs/core/input.cpp
+++ libs/core/input.cpp
@@ -48,57 +48,57 @@ enum class Gesture {
      * Raised when shaken
      */
     //% block=shake
-    Shake = 0, // ACCELEROMETER_EVT_SHAKE,
+    Shake = ACCELEROMETER_EVT_SHAKE,
     /**
      * Raised when the device tilts up
      */
     //% block="tilt up"
-    TiltUp = 1,//ACCELEROMETER_EVT_TILT_UP,
+    TiltUp = ACCELEROMETER_EVT_TILT_UP,
     /**
      * Raised when the device tilts down
      */
     //% block="tilt down"
-    TiltDown = 1,//ACCELEROMETER_EVT_TILT_DOWN,
+    TiltDown = ACCELEROMETER_EVT_TILT_DOWN,
     /**
      * Raised when the screen is pointing left
      */
     //% block="tilt left"
-    TiltLeft = 1,//ACCELEROMETER_EVT_TILT_LEFT,
+    TiltLeft = ACCELEROMETER_EVT_TILT_LEFT,
     /**
      * Raised when the screen is pointing right
      */
     //% block="tilt right"
-    TiltRight = 1,//ACCELEROMETER_EVT_TILT_RIGHT,
+    TiltRight = ACCELEROMETER_EVT_TILT_RIGHT,
     /**
      * Raised when the screen faces up
      */
     //% block="face up"
-    FaceUp = 1,//ACCELEROMETER_EVT_FACE_UP,
+    FaceUp = ACCELEROMETER_EVT_FACE_UP,
     /**
      * Raised when the screen is pointing up and the board is horizontal
      */
     //% block="face down"
-    FaceDown = 1,//ACCELEROMETER_EVT_FACE_DOWN,
+    FaceDown = ACCELEROMETER_EVT_FACE_DOWN,
     /**
      * Raised when the board is falling!
      */
     //% block="free fall"
-    FreeFall = 1,//ACCELEROMETER_EVT_FREEFALL,
+    FreeFall = ACCELEROMETER_EVT_FREEFALL,
     /**
     * Raised when a 3G shock is detected
     */
     //% block="3g"
-    ThreeG = 1,//ACCELEROMETER_EVT_3G,
+    ThreeG = ACCELEROMETER_EVT_3G,
     /**
     * Raised when a 6G shock is detected
     */
     //% block="6g"
-    SixG = 1,//ACCELEROMETER_EVT_6G,
+    SixG = ACCELEROMETER_EVT_6G,
     /**
     * Raised when a 8G shock is detected
     */
     //% block="8g"
-    EightG = 1//ACCELEROMETER_EVT_8G
+    EightG = ACCELEROMETER_EVT_8G
 };
 
 //% noRefCounting fixedInstances

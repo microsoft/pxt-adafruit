59b2eeb08752ed8489b9ccb6db41525819065413
Guillaume Jenkins <gujenkin@microsoft.com>
2017-01-12 14:54:30 -0800

    Set environment variables in Jenkins build

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,19 @@ export NVM_DIR="/home/dotnet-bot/.nvm"
 
 nvm install 5
 
+echo Git branch: $GIT_BRANCH
+
+if [[ "$GIT_BRANCH" =~ ^v([0-9]+\.){2}[0-9]+$ ]]; then
+    echo Setting TRAVIS_TAG to $GIT_BRANCH
+    export TRAVIS_TAG=$GIT_BRANCH
+fi
+
+if [ "$1" == "false" ]; then
+    echo Setting TRAVIS_BRANCH to $GIT_BRANCH
+    export TRAVIS_BRANCH=$GIT_BRANCH
+    echo Setting TRAVIS_PULL_REQUEST to false
+    export TRAVIS_BRANCH=false
+fi
+
 npm install
 npm test

c1a46d2e060a01f7f0445db70922a5ea6039e6fd
Guillaume Jenkins <gujenkin@microsoft.com>
2017-01-11 16:23:07 -0800

    jenkins attempt (#4)

---
+++ jenkins.groovy
@@ -0,0 +1,26 @@
+import jobs.generation.Utilities;
+import jobs.generation.InternalUtilities;
+
+def project = GithubProject
+def branch = GithubBranchName
+
+[true, false].each { isPR ->
+    def newJobName = InternalUtilities.getFullJobName(project, "Default", isPR)
+    def newJob = job(newJobName) {
+        steps {
+            shell("./jenkins.sh")
+        }
+
+        // if (!isPR) {
+        //     wrappers {
+        //         credentialsBinding {
+        //             string('PXT_ACCESS_TOKEN', 'pxt_access_token')
+        //             string('PXT_RELEASE_REPO', 'pxt_release_repo_adafruit')
+        //         }
+        //     }
+        // }
+    }
+
+    Utilities.setMachineAffinity(newJob, 'Ubuntu', '20161020')
+    InternalUtilities.standardJobSetup(newJob, isPR, "*/*")
+}

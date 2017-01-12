89d1d52390b16de20bb7d0c525e9186355902a59
Guillaume Jenkins <gujenkin@microsoft.com>
2017-01-12 15:14:01 -0800

    Updates to Jenkins build (#9)

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Set up NVM
 export NVM_DIR="/home/dotnet-bot/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" 
 
 nvm install 5
+
+echo Git branch: $GIT_BRANCH
+echo isPR: $1
+
+if [[ "$GIT_BRANCH" =~ ^v([0-9]+\.){2}[0-9]+$ ]]; then
+    echo Setting TRAVIS_TAG to $GIT_BRANCH
+    export TRAVIS_TAG=$GIT_BRANCH
+fi
+
+if [ "$1" == "false" ]; then
+    echo Setting TRAVIS_BRANCH to $GIT_BRANCH
+    export TRAVIS_BRANCH=$GIT_BRANCH
+    echo Setting TRAVIS_PULL_REQUEST to false
+    export TRAVIS_BRANCH=false
+fi
 
 npm install
 npm test

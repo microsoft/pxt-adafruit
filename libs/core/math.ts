c75ac53e74461254f823019b406c4fd1aff638fb
Peli de Halleux <jhalleux@microsoft.com>
2017-01-13 21:31:08 -0800

    adding math contrain

--- libs/core/math.ts
+++ libs/core/math.ts
@@ -1,4 +1,4 @@
-namespace math {
+namespace math {    
     /**
      * Re-maps a number from one range to another. That is, a value of ``from low`` would get mapped to ``to low``, a value of ``from high`` to ``to high``, values in-between to values in-between, etc.
      * @param value value to map in ranges
@@ -7,9 +7,23 @@ namespace math {
      * @param toLow the lower bound of the value's target range
      * @param toHigh the upper bound of the value's target range, eg: 4
      */
-    //% help=pins/map weight=23
+    //% help=math/map weight=23 blockGap=8 advanced=true
     //% blockId=math_map block="map %value|from low %fromLow|from high %fromHigh|to low %toLow|to high %toHigh"
     export function map(value: number, fromLow: number, fromHigh: number, toLow: number, toHigh: number): number {
         return ((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow) + toLow;
     }    
+
+    /**
+     * Constrain
+     * Constrains a number to be within a range.
+     * @param x the number to constrain, all data types
+     * @param y the lower end of the range, all data types
+     * @param z the upper end of the range, all data types
+     * @returns x: if x is between a and b, a: if x is less than a, b: if x is greater than b
+     */
+    //% help=math/constraint weight=22 advanced=true
+    //% blockId="arduino_constrain" block="constrain %value|between %low|and %high"
+    export function constrain(value: number, low: number, high: number): number {
+        return value < low ? low : value > high ? high : value;
+    }
 }

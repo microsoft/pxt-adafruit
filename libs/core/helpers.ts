75331e42970778bdc8b95306b082166307102195
Sam El-Husseini <samelh@microsoft.com>
2017-02-28 18:12:46 -0800

    Remove random boolean color

--- libs/core/helpers.ts
+++ libs/core/helpers.ts
@@ -10,7 +10,7 @@ namespace Math {
      * Generates a `true` or `false` value randomly, just like flipping a coin.
      */
     //% blockId=logic_random block="pick random true or false"
-    //% help=math/random-boolean color=230
+    //% help=math/random-boolean
     export function randomBoolean(): boolean {
         return Math.random(2) == 0;
     }

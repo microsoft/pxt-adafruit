33012441bc5d99c89485772e432e7d8861f05eb2
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-04-18 16:26:13 -0700

    Added extensions (#385)
    * inject file importers as extensions
    
    * logging
    
    * updated signature

---
+++ editor/extension.ts
@@ -0,0 +1,28 @@
+/// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts" />
+
+namespace pxt.editor {
+    initExtensionsAsync = function(opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
+        pxt.debug('loading microbit target extensions...')
+        const res: pxt.editor.ExtensionResult = {
+            hexFileImporters: [{
+                id: "blockly",
+                canImport: data => data.meta.cloudId == "microbit.co.uk" && data.meta.editor == "blockly",
+                importAsync: (project, data) => project.createProjectAsync({
+                    filesOverride: {
+                        "main.blocks": data.source
+                    }, name: data.meta.name
+                })
+            }, {
+                    id: "td",
+                    canImport: data => data.meta.cloudId == "microbit.co.uk" && data.meta.editor == "touchdevelop",
+                    importAsync: (project, data) =>
+                        project.createProjectAsync({
+                            filesOverride: { "main.blocks": "", "main.ts": "  " },
+                            name: data.meta.name
+                        }).then(() => project.convertTouchDevelopToTypeScriptAsync(data.source))
+                        .then(text => this.textEditor.overrideFile(text))
+                }]
+        };
+        return Promise.resolve<pxt.editor.ExtensionResult>(res);
+    }
+}

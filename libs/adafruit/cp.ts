b96c0e9dd01ab676abc545f9295b0fbe1aeb1a70
Peli de Halleux <jhalleux@microsoft.com>
2017-05-01 15:34:42 -0700

    simple api layer

--- libs/adafruit/cp.ts
+++ libs/adafruit/cp.ts
@@ -1,16 +1,16 @@
 enum CircuitPlaygroundEvent {
-    LeftButtonClicked,
-    RightButtonClicked,
+    LeftButtonIsClicked,
+    RightButtonIsClicked,
     Shake,
-    TiltUp,
-    TiltDown,
-    TiltLeft,
-    TiltRight,
+    TiltedUp,
+    TiltedDown,
+    TiltedLeft,
+    TiltedRight,
     Loud,
-    Quiet
+    RoomIsQuiet
 }
 
-enum CircuitPlaygroundSensor {
+enum CircuitPlaygroundInput {
     AccelerationX,
     AccelerationY,
     AccelerationZ,
@@ -18,23 +18,51 @@ enum CircuitPlaygroundSensor {
     Light
 }
 
+enum CircuitPlaygroundOutput {
+    //% block="rainbow animation"
+    RainbowAnimation,
+    SparkleAnimation,
+    CometAnimation,
+    AllRed,
+    AllBlue,
+    AllGreen,
+    AllOrange
+}
+
+enum CircuitPlaygroundPaint {
+    //% block="forward"
+    Forward,
+    //% block="backward"
+    Backward,
+    //% block="stamp"
+    Stamp,
+    //% block="pen up"
+    PenUp,
+    //% block="pen down"
+    PenDown,
+    //% block="eraser"
+    Eraser,
+    //% block="clear"
+    Clear
+}
+
 /**
- * Circuit Playground Junior blocks
+ * Circuit Playground blocks
  * 
  */
 //% color=#E2008C icon="\uf0a3"
-namespace cp {
+namespace circuitPlayground {
     /**
      * Register code for an event on the Circuit Playground
      */
-    //% blockId=cp_on_event block="on %event" weight=99
-    export function onEvent(event: CircuitPlaygroundEvent, handler: Action) {
+    //% blockId=cp_on_event block="when %event" weight=99 blockGap=8
+    export function when(event: CircuitPlaygroundEvent, handler: Action) {
         const cb = () => handler();
         switch (event) {
-            case CircuitPlaygroundEvent.LeftButtonClicked:
+            case CircuitPlaygroundEvent.LeftButtonIsClicked:
                 input.leftButton.onEvent(ButtonEvent.Click, cb);
                 break;
-            case CircuitPlaygroundEvent.RightButtonClicked:
+            case CircuitPlaygroundEvent.RightButtonIsClicked:
                 input.rightButton.onEvent(ButtonEvent.Click, cb);
                 break;
             case CircuitPlaygroundEvent.Shake:
@@ -46,13 +74,58 @@ namespace cp {
     /**
      * Reads sensor data from one of the sensors
      */
-    //% blockId=cp_sense block="sense %sensor" weight=98
-    export function sense(sensor: CircuitPlaygroundSensor): number {
+    //% blockId=cp_read block="read %sensor" weight=98 blockGap=8
+    export function read(sensor: CircuitPlaygroundInput): number {
         switch (sensor) {
-            case CircuitPlaygroundSensor.AccelerationX: return input.acceleration(Dimension.X);
-            case CircuitPlaygroundSensor.AccelerationY: return input.acceleration(Dimension.Y);
-            case CircuitPlaygroundSensor.AccelerationZ: return input.acceleration(Dimension.Z);
+            case CircuitPlaygroundInput.AccelerationX: return input.acceleration(Dimension.X);
+            case CircuitPlaygroundInput.AccelerationY: return input.acceleration(Dimension.Y);
+            case CircuitPlaygroundInput.AccelerationZ: return input.acceleration(Dimension.Z);
             default: return 0;
         }
+    }
+
+    /**
+     * Controls the painter photon
+     * 
+     */
+    //% blockId=cp_paint block="paint %action" blockGap=8
+    export function paint(action: CircuitPlaygroundPaint) {
+        
+    }
+
+    /**
+     * Changes the pen color used in paint
+     */
+    //% blockId=cp_set_pen_color block="set pen color %color" blockGap=8
+    export function setPenColor(color: number) {
+    }
+
+    /**
+     * Shows an animation on the LED screen
+     */
+    //% blockId=cp_show block="show %out" weight=90 blockGap=8
+    export function show(out: CircuitPlaygroundOutput) {
+        const cycles = 25;
+        let anim: light.NeoPixelAnimation = null;
+        switch (out) {
+            case CircuitPlaygroundOutput.RainbowAnimation: anim = light.rainbowAnimation(); break;
+            case CircuitPlaygroundOutput.CometAnimation: anim = light.cometAnimation(); break;
+        }
+        if (anim) {
+            for (let i = 0; i < cycles; ++i) {
+                light.pixels.showAnimationFrame(anim);
+                control.pause(50);
+            }
+            light.pixels.clear();
+            light.pixels.show();
+        }
+    }
+
+    /**
+     * Plays a tone at the given frequency
+     */
+    //% blockId=cp_play_note block="play tone|at %note=device_note" blockGap=8
+    export function playTone(frequency: number) {
+        pins.A8.playTone(frequency, music.beat() / 4);
     }
 }

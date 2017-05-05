23aabbc08a9df2de56f8ad78833974e2df3cff39
Peli de Halleux <jhalleux@microsoft.com>
2017-05-04 21:16:02 -0700

    better showring

--- libs/adafruit/ns.ts
+++ libs/adafruit/ns.ts
@@ -5,17 +5,24 @@ namespace input {
 
 namespace light {
 
+    /**
+     * Shows a pattern of colors on the pixel ring
+     */
     //% blockId="neopixel_show_ring_colors" block="show ring |%array"
     //% weight=100 blockGap=8
     //% parts="neopixel" 
     //% array.fieldEditor="lights" 
     //% defaultInstance=light.pixels blockExternalInputs="true"
     export function showRing(colors: number[], interval: number = 400) {
-        const strip : light.NeoPixelStrip = light.pixels;
-        for(let i = 0; i < colors.length; i++) {
-            strip.setPixelColor(i, light.colors(colors[i]))
+        const strip = light.pixels;
+        const n = Math.min(strip.length(), colors.length);
+        const bf = strip.buffered();
+        strip.setBuffered(true);
+        for(let i = 0; i < n; i++) {
+            strip.setPixelColor(i, colors[i])
         }
         strip.show();
-        control.pause(interval);
+        strip.setBuffered(bf);
+        loops.pause(interval);
     }
 }

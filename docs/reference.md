992fe7b7ce020ec1e74e0aef8f7e5717cde2ac24
ganicke <v-ganick@microsoft.com>
2017-05-17 17:01:48 -0700

    Fixup stuff for card pages

--- docs/reference.md
+++ docs/reference.md
@@ -1,13 +1,11 @@
 # Reference
 
 ```namespaces
-light.showLeds(`red red red red red red red red red red red`);
 input.onGesture(Gesture.Shake, () => {})
-music.startMelody([], MelodyOptions.Once)
-control.runInBackground(() => {
+light.showRing('red red red red red red red red red red')
 music.playTone(0, 0)
-});
 pins.A1.servoWrite(0)
+control.runInBackground(() => {})
 serial.writeLine("");
 ```
 
--- libs/music/shims.d.ts
+++ libs/music/shims.d.ts
@@ -2,9 +2,10 @@
 declare namespace music {
 
     /**
-     * Sets the PCM sample (1024 x 10bit unsigned samples) used to generate the tones.
+     * Set a source of digital sound data (PCM) for making tones.
+     * Samples are 1020 x 10bit unsigned PCM.
      * A reference to the buffer is kept to avoid the memory overhead, so changes to the buffer
-     * values will be reflected live in the sound output. 
+     * values are reflected immediately to the sound output. 
      */
     //% help=music/set-tone
     //% weight=1 advanced=true
@@ -12,18 +13,19 @@ declare namespace music {
     function setTone(buffer: Buffer): void;
 
     /**
-     * Sets the output volume of the synthesizer
+     * Set the output volume of the sound synthesizer.
      * @param volume the volume 0...256, eg: 128
      */
     //% weight=96
     //% blockId=synth_set_volume block="set volume %volume"
     //% parts="speaker" blockGap=8
     //% volume.min=0 volume.max=256
+    //% help=music/set-volume
     //% weight=1 shim=music::setVolume
     function setVolume(volume: int32): void;
 
     /**
-     * Plays a tone through the pin for the given duration.
+     * Play a tone through the speaker for some amount of time.
      * @param frequency pitch of the tone to play in Hertz (Hz)
      * @param ms tone duration in milliseconds (ms)
      */

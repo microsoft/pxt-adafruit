934d8c97b469e9f3c83f5147105d3eb9c14f3e67
guillaumejenkins <gujenkin@microsoft.com>
2017-06-29 14:20:05 -0700

    Improve error page

--- pxtwapp/pxtwapp/msapp-error.js
+++ pxtwapp/pxtwapp/msapp-error.js
@@ -1,22 +1,35 @@
-﻿(function () { 
-  var validParameterNames = [ "httpStatus", "failureName", "failureUrl" ]; 
-  
-  function parseQueryParameters() { 
-    var query = location.search.slice(1); 
-    return query.split("&").reduce(function (queryParameters, rawPair) {
-                                   var pair = rawPair.split("=").map(decodeURIComponent);
-                                   queryParameters[pair[0]] = pair[1];
-                                   return queryParameters;
-    }, {});
-  }
- 
-  function initialize() {
-    var queryParameters = parseQueryParameters();
-    validParameterNames.forEach(function (parameterName) {
-                               var parameterValue = queryParameters[parameterName] || "N/A"; 
-                               document.getElementById(parameterName + "Value").textContent = parameterValue;
-    });
-  }
-  
-  document.addEventListener("DOMContentLoaded", initialize);
+﻿(function () {
+    var validParameterNames = ["httpStatus", "failureName", "failureUrl"];
+    //var startUrl = "https://makecode.adafruit.com/app/0d3abd96cb0441f42443cfef5e961dd06220f2ec-a7804babf6";
+    var startUrl = "https://makecode.adafruit.com";
+
+    function parseQueryParameters() {
+        var query = location.search.slice(1);
+        return query.split("&").reduce(function (queryParameters, rawPair) {
+            var pair = rawPair.split("=").map(decodeURIComponent);
+            queryParameters[pair[0]] = pair[1];
+            return queryParameters;
+        }, {});
+    }
+
+    function initialize() {
+        document.getElementById("retryButton").addEventListener("click", (e) => {
+            window.location.href = startUrl;
+        });
+        var queryParameters = parseQueryParameters();
+        validParameterNames.forEach(function (parameterName) {
+            var parameterValue = queryParameters[parameterName] || "N/A";
+            document.getElementById(parameterName + "Value").textContent = parameterValue;
+        });
+    }
+
+    function updateOnlineStatus(e) {
+        history.back();
+        if (history.length == 1) {
+            window.location.href = startUrl;
+        }
+    }
+
+    window.addEventListener("online", updateOnlineStatus);
+    document.addEventListener("DOMContentLoaded", initialize);
 }());

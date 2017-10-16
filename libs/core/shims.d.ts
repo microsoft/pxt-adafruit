48da5daaedb5cd8d131c3f790ac61212c94628d8
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-10-15 21:53:36 -0700

    Revert "i2c package shims and update (#362)"
    This reverts commit 9b673d7c5b4f9d65639bbe0696731e47495f057f.

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -228,5 +228,19 @@ declare namespace control {
     //% help=control/allocate-notify-event shim=control::allocateNotifyEvent
     function allocateNotifyEvent(): int32;
 }
+declare namespace pins {
+
+    /**
+     * Read `size` bytes from a 7-bit I2C `address`.
+     */
+    //% repeat.defl=0 shim=pins::i2cReadBuffer
+    function i2cReadBuffer(address: int32, size: int32, repeat?: boolean): Buffer;
+
+    /**
+     * Write bytes to a 7-bit I2C `address`.
+     */
+    //% repeat.defl=0 shim=pins::i2cWriteBuffer
+    function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+}
 
 // Auto-generated. Do not edit. Really.

eae0ac74ce4f9da069ce1dbdf5936d81aea5fb0e
Sam El-Husseini <samelh@microsoft.com>
2017-10-18 23:53:35 -0700

    Better default parameters in JS for animation and music functions

--- libs/circuit-playground/ns.ts
+++ libs/circuit-playground/ns.ts
@@ -159,7 +159,7 @@ namespace light {
 
     /**
      * Show an animation or queue an animation in the animation queue
-     * @param animation the animation to run
+     * @param animation the animation to run, eg: light.animation(LightAnimation.Rainbow)
      * @param duration the duration to run in milliseconds, eg: 500
      */
     //% blockId=builtin_neopixel_show_animation block="show %animation=light_animation|animation for %duration=timePicker|ms"
@@ -171,7 +171,7 @@ namespace light {
 
     /**
      * Show a single animation frame
-     * @param animation the animation to run
+     * @param animation the animation to run, eg: light.animation(LightAnimation.Rainbow)
      */
     //% blockId=builtin_neopixel_show_animation_frame block="show animation frame %animation=light_animation"
     //% help="light/show-animation-frame"

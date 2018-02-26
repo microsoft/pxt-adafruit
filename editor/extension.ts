1725d83b0741107853042e123d4f6d849960afd5
Guillaume Jenkins <gujenkin@microsoft.com>
2018-02-26 12:25:41 -0500

    Move logic to pxt-core and use a flag in target

--- editor/extension.ts
+++ editor/extension.ts
@@ -1,13 +1,8 @@
 /// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts" />
-/// <reference path="../node_modules/pxt-core/built/pxtwinrt.d.ts"/>
 
 import lf = pxt.Util.lf;
 
 namespace pxt.editor {
-    // These are the VID and PID of the default program that ships on brand new CPX devices
-    const cpxDefaultVid = 0x239A;
-    const cpxDefaultPid = 0x8018;
-
     initExtensionsAsync = function (opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
         pxt.debug('loading pxt-adafruit target extensions...')
         const res: pxt.editor.ExtensionResult = {
@@ -96,42 +91,6 @@ namespace pxt.editor {
                     } : undefined]
                     //timeout: 20000
                 }).then(() => { });
-            },
-            winrtHandleDeviceNotFoundAsync: (io: pxt.winrt.WindowsRuntimeIO) => {
-                // Device not found while deploying. It could be because it's a fresh device (no MakeCode program
-                // running) that is in non-bootloader mode. Try to connect to that device via serial and set baud to
-                // 1200, which sets the device into bootloader.
-                let allSerialDevices: Windows.Devices.SerialCommunication.SerialDevice[];
-                const defaultCpxHid: pxtc.HidSelector = {
-                    vid: cpxDefaultVid.toString(),
-                    pid: cpxDefaultPid.toString(),
-                    usageId: undefined,
-                    usagePage: undefined
-                };
-                return pxt.winrt.connectSerialDevicesAsync([defaultCpxHid])
-                    .then((serialDevices) => {
-                        if (!serialDevices || serialDevices.length === 0) {
-                            // No device found, it really looks like no device is plugged in. Bail out.
-                            return Promise.reject(new Error("no serial devices to switch into bootloader"));
-                        }
-
-                        // CPX with default program connected to; attempt to switch to bootloader using the baud trick
-                        allSerialDevices = serialDevices;
-                        allSerialDevices.forEach((dev) => {
-                            dev.baudRate = 1200;
-                            dev.close();
-                        });
-                        // A long delay is needed before attempting to connect to the bootloader device, enough for the
-                        // OS to recognize the device has been plugged in. Note that without drivers, connection to the
-                        // device might still fail the first time, but drivers should be installed by the time the user
-                        // clicks Download again, at which point flashing will work (without ever needing to press the
-                        // reset button)
-                        return Promise.delay(1500);
-                    })
-                    .then(() => {
-                        // Try to connect to the HID device again
-                        return io.initAsync(/* isRetry */ true);
-                    })
             }
         };
         return Promise.resolve<pxt.editor.ExtensionResult>(res);

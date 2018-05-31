427a56c2753aa344aaf4b3004229782b2217d538
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-31 13:49:53 -0700

    fixing export

--- libs/servo/targetoverrides.ts
+++ libs/servo/targetoverrides.ts
@@ -1,6 +1,6 @@
 namespace motors {
     //% block="servo A1" fixedInstance
-    const servoA1 = new motors.PinServo(pins.A1);
+    export const servoA1 = new motors.PinServo(pins.A1);
     //% block="servo A1" fixedInstance
-    const servoA2 = new motors.PinServo(pins.A2);
+    export const servoA2 = new motors.PinServo(pins.A2);
 }

c363cf94ed41d334eb53be89e77d420097d34516
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-01 09:02:44 -0700

    Nicer servo support (#733)
    * update "pixels" definition
    
    * adding servo abstraction
    
    * adding servo lib
    
    * adding servo library
    
    * fixing export
    
    * updated packages

---
+++ libs/servo/targetoverrides.ts
@@ -0,0 +1,6 @@
+namespace motors {
+    //% block="servo A1" fixedInstance
+    export const servoA1 = new motors.PinServo(pins.A1);
+    //% block="servo A1" fixedInstance
+    export const servoA2 = new motors.PinServo(pins.A2);
+}

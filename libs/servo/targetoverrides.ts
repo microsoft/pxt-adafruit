3226ba5584486b72e262a8cfbc46b4959c0855c0
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-03-19 08:16:14 -0700

    enable all servos

--- libs/servo/targetoverrides.ts
+++ libs/servo/targetoverrides.ts
@@ -3,4 +3,10 @@ namespace servos {
     export const A1 = new servos.PinServo(pins.A1);
     //% block="servo A2" fixedInstance
     export const A2 = new servos.PinServo(pins.A2);
+    //% block="servo A3" fixedInstance
+    export const A3 = new servos.PinServo(pins.A3);
+    //% block="servo A6" fixedInstance
+    export const A6 = new servos.PinServo(pins.A6);
+    //% block="servo A7" fixedInstance
+    export const A7 = new servos.PinServo(pins.A7);
 }

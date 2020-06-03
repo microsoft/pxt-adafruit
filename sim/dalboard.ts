94063d3c0e7750eff3d8dc6ba4fb16dbd9e911a1
peli <pelikhan@users.noreply.github.com>
2020-06-03 16:02:21 -0700

    include dal file

--- sim/dalboard.ts
+++ sim/dalboard.ts
@@ -1,6 +1,7 @@
 /// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
 /// <reference path="../node_modules/pxt-core/localtypings/pxtarget.d.ts"/>
 /// <reference path="../built/common-sim.d.ts"/>
+/// <reference path="../libs/core/dal.d.ts"/>
 
 namespace pxsim {
     export module CPlayPinName {
